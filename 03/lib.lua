53bd3ba2b462570cb03bb76395f18e7d4d466fd4
Simon Samuel <salviensky@gmail.com>
2026-07-08 19:59:13 +0100

    feat: solved challenge Day 03 [Part 02]

--- 03/lib.lua
+++ 03/lib.lua
@@ -6,33 +6,44 @@ function M.process(data)
     ---@type integer
     local result = 0
 
-    for line in data:gmatch("([^\n]*)\n?") do
-        ---@type integer
-        local m = 0
+    for line in data:gmatch("[^\n]+") do
+        ---@type string
+        local digits = ""
 
-        ---@type integer
-        local n = line:len()
+        ---@type integer, integer
+        local p, q = 1, line:len() - 11
 
-        for i = 1, n do
-            for j = i + 1, n do
-                local digits = line:sub(i, i) .. line:sub(j, j)
+        while q <= line:len() do
+            ---@type string
+            local window = line:sub(p, q)
 
+            ---@type integer, integer
+            local m, n = -1, window:len()
+            local step = 1
+
+            for i = 1, n do
                 ---@type number?
-                local k = tonumber(digits)
+                local k = tonumber(window:sub(i, i))
                 if k == nil then
-                    print(string.format("Invalid input: %s", digits))
+                    print(string.format("Invalid input: %s", window:sub(i, i)))
                     goto continue
                 end
 
                 if k > m then
+                    step = i
                     m = k
                 end
+
+                ::continue::
             end
 
-            ::continue::
+            p = p + step
+            digits = digits .. tostring(m)
+
+            q = q + 1
         end
 
-        result = result + m
+        result = result + (tonumber(digits) or 0)
     end
 
     return result
--- 03/test_main.lua
+++ 03/test_main.lua
@@ -7,10 +7,10 @@ end
 
 function test_main()
     local data = "987654321111111\n811111111111119\n234234234234278\n818181911112111\n"
-    local expected = 357
+    local expected = 3121910778619
     local actual = mod.process(data)
 
-    lu.assertEquals(expected, actual)
+    lu.assertEquals(actual, expected)
 end
 
 os.exit(lu.LuaUnit.run())
