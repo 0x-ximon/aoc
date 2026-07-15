33c6c43dd75c549ec84308224616e2a388617598
Simon Samuel <salviensky@gmail.com>
2026-07-15 10:05:22 +0100

    feat: solved Day 04 [Part 02]

--- 2025/04/lib.lisp
+++ 2025/04/lib.lisp
@@ -41,4 +41,34 @@
         (m (list-length data))
         (n (length (nth 0 data))))
 
+    (loop named forever do
+          (let ((removed 0)
+                (next (mapcar #'copy-seq data)))
+
+            (loop for i from 0 to (- m 1) do
+                  (loop for j from 0 to (- n 1) do
+                        (let ((count 0)
+                              (c (char (nth i data) j)))
+
+                          (when (char= c #\@)
+                            (loop for dir in *directions* do
+                                  (let ((x (+ i (nth 0 dir)))
+                                        (y (+ j (nth 1 dir))))
+
+                                    (when (and (>= x 0) (< x m)
+                                               (>= y 0) (< y n))
+
+                                      (let ((d (char (nth x data) y)))
+                                        (when (char= d #\@)
+                                          (incf count))))))
+
+                            (when (< count 4)
+                              (setf (char (nth i next) j) #\.)
+                              (incf removed)
+                              (incf result))))))
+
+            (setf data next)
+            (when (= removed 0)
+              (return-from forever))))
+
     result))
